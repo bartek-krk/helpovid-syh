6df6e1c567220226ac55f2d5df00c04bad259c97
bartek-krk <bartosz.lukasik@icloud.com>
2020-11-20 18:33:37 +0100

    init commit

---
+++ src/main/java/pl/ddcrew/helpovid/HelpovidApplication.java
@@ -0,0 +1,13 @@
+package pl.ddcrew.helpovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ddcrew/helpovid/HelpovidApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ddcrew.helpovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
